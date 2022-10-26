4d87f6bee09df4cfc3b87d557629a85cad128fe0
ShamsutdinovAnvar <a.shamsutdinov2021@gmail.com>
2022-10-26 09:15:08 +0500

    second commit

--- src/test/java/com/wegotrip/tests/RegistrationFormTest.java
+++ src/test/java/com/wegotrip/tests/RegistrationFormTest.java
@@ -1,25 +1,52 @@
 package com.wegotrip.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.wegotrip.pages.RegistrationFormPage;
 import com.wegotrip.pages.testdata.UserTestData;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import com.wegotrip.Attach;
+
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationFormTest {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
-        @BeforeAll
-        static void setUp() {
-            Configuration.baseUrl = "https://demoqa.com";
-            Configuration.browserSize = "1920x1080";
-            Configuration.holdBrowserOpen = true;
-        }
+    @BeforeAll
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
 
         @Test
         void checkFormTest() {
-            registrationFormPage.openPage()
-                    .setFirstName(UserTestData.firstName)
+            step("Open registrations form", () -> {
+            registrationFormPage.openPage();
+            });
+            step("Fill form", () -> {
+            registrationFormPage.setFirstName(UserTestData.firstName)
                     .setLastName(UserTestData.lastName)
                     .setUserEmail(UserTestData.email)
                     .setGender(UserTestData.gender)
@@ -30,7 +57,9 @@ public class RegistrationFormTest {
                     .uploadPicture(UserTestData.picture)
                     .setAddress(UserTestData.address, UserTestData.state, UserTestData.city)
                     .clickSubmit();
+            });
             //Check results
+            step("Check form results", () -> {
             registrationFormPage.checkResultsTable()
                     .checkResult("Student Name", UserTestData.firstName + " " + UserTestData.lastName)
                     .checkResult("Student Email", UserTestData.email)
@@ -42,6 +71,7 @@ public class RegistrationFormTest {
                     .checkResult("Picture", UserTestData.picture)
                     .checkResult("Address", UserTestData.address)
                     .checkResult("State and City", UserTestData.state + " " + UserTestData.city);
+            });
         }
     }
 
